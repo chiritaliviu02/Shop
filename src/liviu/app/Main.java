8f686ee9382f6db9d88270006f17a38dd1c6ab89
Chirita Liviu <chiritaliviu02@gmail.com>
2022-03-28 19:55:43 +0300

    created another Product, get the values for each Product

--- src/liviu/app/Main.java
+++ src/liviu/app/Main.java
@@ -1,7 +1,9 @@
 package liviu.app;
 import java.math.BigDecimal;
+import java.util.Formatter.BigDecimalLayoutForm;
 
 import liviu.data.Product;
+import liviu.data.Rating;
 
 
 public class Main 
@@ -9,13 +11,18 @@ public class Main
 
 	public static void main(String[] args) 
 	{
-		Product p1 = new Product();
-		p1.setId(101);
-	    p1.sePtice(BigDecimal.valueOf(1.99));
-		p1.setName("Tea");
-		System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount());
+		Product p1 = new Product(101, "Tea", BigDecimal.valueOf(1.99) );
+//		p1.setId(101);
+//	    p1.sePtice(BigDecimal.valueOf(1.99));
+//		p1.setName("Tea");
 		
-	
+		Product p2 = new Product(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR );
+		Product p3 = new Product(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR);
+		Product p4 = new Product();
+		System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount() + " " + p1.getRatig().getStars());
+		System.out.println(p2.getId() + " " + p2.getName() + " " + p2.getPrice() + " " + p2.getDiscount() + " " + p2.getRatig().getStars());
+		System.out.println(p3.getId() + " " + p3.getName() + " " + p3.getPrice() + " " + p3.getDiscount() + " " + p3.getRatig().getStars());
+		System.out.println(p4.getId() + " " + p4.getName() + " " + p4.getPrice() + " " + p4.getDiscount() + " " + p4.getRatig().getStars());
 	}
 
 }
--- src/liviu/data/Product.java
+++ src/liviu/data/Product.java
@@ -4,7 +4,8 @@
 package liviu.data;
 
 import java.math.BigDecimal;
-import  static java.math.RoundingMode.HALF_UP;
+import static java.math.RoundingMode.HALF_UP;
+import static liviu.data.Rating.*;
 
 /**
  * @author 40751
@@ -16,6 +17,24 @@ public class Product {
 	private BigDecimal price;
 	public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
 	private Rating rating;
+	
+	public Product () 
+	{
+		this(0, "no name", BigDecimal.ZERO);
+	}
+	
+	 public Product (int id, String name, BigDecimal price, Rating rating) 
+	 {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.rating = rating;
+	 }
+	 
+	 public Product (int id, String name, BigDecimal price)
+	 {
+		this(id, name, price, NOT_RATED); 
+	 }
 	
 	public int getId() {
 		return id;
