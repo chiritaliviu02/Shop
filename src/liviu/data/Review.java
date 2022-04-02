9d3a1257ee5aeb16bbe0e7b7938d562754037606
Chirita Liviu <chiritaliviu02@gmail.com>
2022-04-02 21:54:04 +0300

    created resource boundle

---
+++ src/liviu/data/Review.java
@@ -0,0 +1,40 @@
+/**
+   * Author: Liviu
+ */
+package liviu.data;
+
+/**
+ * @author 40751
+ *
+ */
+public class Review {
+	private Rating rating;
+	private String comments;
+	
+	public Review(Rating rating, String comments)
+	{
+		
+		this.rating = rating;
+		this.comments = comments;
+		
+	}
+	
+	public Rating getRating()
+	{
+		return rating;
+	}
+	
+	public String getComments()
+	{
+		return comments;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "Review( " + "rating=" + rating + ", comments=" + comments + ")"; 
+	}
+	
+	
+	
+}
