5f91505af7e802fe215130cd82806d160420d5a7
Chirita Liviu <chiritaliviu02@gmail.com>
2022-04-05 16:38:45 +0300

    change Product Manager class as a Map

--- src/liviu/data/ProductManager.java
+++ src/liviu/data/ProductManager.java
@@ -17,13 +17,13 @@ import java.util.*;
  */
 public class ProductManager {
 	
-	private Product product;
-	private Review[] reviews = new Review[5];
+//	private Product product;
+//	private Review[] reviews = new Review[5];
 	private Locale locale;
 	private ResourceBundle resources;
 	private DateTimeFormatter dateFormat;
 	private NumberFormat moneyFormat;
-	
+	private Map<Product, List<Review>> products = new HashMap<>();
 	
 	public ProductManager(Locale locale)
 	{
@@ -36,42 +36,45 @@ public class ProductManager {
 	
 	public Product createProduct(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore)
 	{
-		product = new Food(id, name, price, rating, bestBefore );
+		 Product product = new Food(id, name, price, rating, bestBefore );
+		 products.putIfAbsent(product, new ArrayList<>());
 		return product;
 	}
 	
-	public Product createProduct(int id, String name, BigDecimal price, Rating rating)
+	public Product createProduct(int id, String name, BigDecimal price, Rating rating) 
 	{
-		product = new Drink(id, name, price, rating  );
+		Product product = new Drink(id, name, price, rating  );
+		 products.putIfAbsent(product, new ArrayList<>());
 		return product;
 	} 
 	
 	public Product reviewProduct(Product product, Rating rating, String comments)
 	{
 		
-		if(reviews[reviews.length -1] != null )
+//		if(reviews[reviews.length -1] != null )
+//		{
+//			reviews = Arrays.copyOf(reviews, reviews.length + 5	);
+//		}
+		List<Review> reviews = products.get(product);
+		products.remove(product, reviews);
+		reviews.add(new Review(rating, comments));
+		int sum = 0;
+		
+		for(Review review : reviews)
 		{
-			reviews = Arrays.copyOf(reviews, reviews.length + 5	);
+			sum += review.getRating().ordinal();
 		}
-		int sum = 0, i = 0;  
-		boolean reviewed = false;
-		while (i < reviews.length && !reviewed) 
-		{
-			if(reviews[i] == null)
-			{
-				reviews[i] = new Review(rating, comments);
-				reviewed = true;
-			}
-			sum += reviews[i].getRating().ordinal();
-			i++;
-		}
-		this.product = product.applyRating(Rateable.convert(Math.round((float) sum / i)));
-		return this.product;
+		
+
+		product = product.applyRating(Rateable.convert(Math.round((float) sum / reviews.size())));
+		products.put(product, reviews);
+		return product;
 		
 	}
 	 
-	public void printProductReport()
+	public void printProductReport(Product product)
 	{
+		List<Review> reviews = products.get(product);
 		 StringBuilder txt = new StringBuilder();
 		 txt.append(MessageFormat.format(resources.getString("product"),
 		 product.getName(),
@@ -93,7 +96,7 @@ public class ProductManager {
 			 txt.append('\n');
 		 }
 		 
-		if(reviews[0] == null)
+		if(reviews.isEmpty())
 		{
 			txt.append(resources.getString("no.reviews"));
 			txt.append('\n');
