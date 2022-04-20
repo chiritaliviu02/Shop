bc30e08e472f0b0ba3cdea2fd1cbd00eb403f9c4
Chirita Liviu <chiritaliviu02@gmail.com>
2022-04-20 15:53:03 +0300

    Manage Main method to have objects safe thared

--- src/liviu/app/Main.java
+++ src/liviu/app/Main.java
@@ -1,11 +1,20 @@
 package liviu.app;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import liviu.data.Product;
 import liviu.data.ProductManager;
 import liviu.data.Rating;
 
@@ -34,18 +43,31 @@ public class Main
 					.stream()
 					.map(entry -> entry.getKey() + "\t" + entry.getValue())
 					.collect(Collectors.joining("\n")));
-			log.append("\n-\tend of log\t-\n");
-			
+			Product product = pm.reviewProduct(productId, Rating.FOUR_STAR, " inca o parere");
+			log.append((product != null) ? "\n Produs " + productId + "catalogat\n" : "\nProdus " + productId + " nu este catalogat\n");
+			pm.printProductReport(productId, languageTag, clientId);
+			log.append(clientId + "raport generat pentru produs " + productId);
+			log.append("\n-\tend of log\t-\n");			
 			return log.toString();
 		};
 		
+		List<Callable<String>> clients = Stream.generate(() -> client).limit(5).collect(Collectors.toList());
+		ExecutorService executorSerivice = Executors.newFixedThreadPool(3);
+		try {
+			List<Future<String>> results = executorSerivice.invokeAll(clients);
+			executorSerivice.shutdown();
+			results.stream().forEach(result ->{
+				try {
+					System.out.println(result.get());
+				} catch (InterruptedException  | ExecutionException ex) {
+					Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Eror retrieving client log ", ex);
+				}
+			});
+		} catch (InterruptedException ex) {
+			
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Error invoking clients ", ex);
+		}
 		
-		
-		
-		
-		
-		pm.printProductReport(101, "en-GB");
-		pm.printProductReport(103, "ro-RO");
 		
 	}
 
